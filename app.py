a148a2fed983eb15fd8d12b18298f42ad6454caa
Jan N Rose <janniklas.rose@gmail.com>
2022-05-01 16:40:23 +0200

    Use HTML template

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Flask front-end app for magicframe"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 HOST="0.0.0.0"
 PORT=8081
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
 def toggle():
-    return "<h1>MagicFrame</h1>"
+    return render_template("index.html", toggle_message="Turn display on/off")
 
 
 if __name__ == "__main__":
