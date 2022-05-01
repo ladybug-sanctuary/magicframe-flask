dab8a24143ea25ec43a95222132cb33cf5f2d189
Jan N Rose <janniklas.rose@gmail.com>
2022-05-01 16:42:34 +0200

    Support POST requests

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask front-end app for magicframe"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 HOST="0.0.0.0"
@@ -10,8 +10,14 @@ PORT=8081
 app = Flask(__name__, template_folder="templates")
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def toggle():
+    if request.method == "GET":
+        pass
+
+    if request.method == "POST":
+        pass
+
     return render_template("index.html", toggle_message="Turn display on/off")
 
 
