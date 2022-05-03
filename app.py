c20da6f693c27d3d8e3b2fa90e119f8b66de6032
Jan N Rose <janniklas.rose@gmail.com>
2022-05-03 22:29:19 +0200

    Do not hard code button text, only pass status

--- app.py
+++ app.py
@@ -12,24 +12,24 @@ PORT=8081
 app = Flask(__name__, template_folder="templates")
 
 
-def turn_display(newstatus: int) -> str:
-    return "Turn display " + ("on" if newstatus else "off")
+def on_or_off(newstatus: int) -> str:
+    return "on" if newstatus else "off"
 
 
 @app.route("/", methods=["GET", "POST"])
 def toggle():
     if request.method == "GET":
         newstatus = vcgencmd.get_status() ^ 1  # 1 or 0
-        return render_template("index.html", toggle_message=turn_display(newstatus))
+        return render_template("index.html", on_or_off=on_or_off(newstatus))
 
     if request.method == "POST":
         if "toggle" in request.form:
             oldmsg = request.form.get("toggle")  # previous value of toggle_message
             action = oldmsg.split()[-1]
-            newstatus = 1 if action == "on" else 0  # "on" vs "off"
+            newstatus = 1 if action.lower() == "on" else 0  # "on" vs "off"
             vcgencmd.set_status(newstatus)
             newstatus = int(newstatus) ^ 1  # flip after executing command
-            return render_template("index.html", toggle_message=turn_display(newstatus))
+            return render_template("index.html", on_or_off=on_or_off(newstatus))
 
     return render_template("index.html")
 
